474cc811143df1f8b2731d26a306e33693cf1ea2
NG <bbcn419a@gmail.com>
2024-05-22 22:47:53 +0300

    be - book model -m -c --resource created

---
+++ backend/app/Http/Controllers/BookController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        $books = Book::all();
+        return response()->json(['book' => $books], 200);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Book $book)
+    {
+        //
+    }
+
+    public function edit(Book $book)
+    {
+        //
+    }
+
+
+    public function update(Request $request, Book $book)
+    {
+        //
+    }
+
+    public function destroy(Book $book)
+    {
+        //
+    }
+}
