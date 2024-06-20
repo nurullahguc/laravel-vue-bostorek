09b7c95127131977e9d7461a1e8291b2f47d5a60
NG <bbcn419a@gmail.com>
2024-06-20 12:21:47 +0300

    fe -  #57/3 BooksView.vue Options API => Composition API

--- frontend/src/views/BooksView.vue
+++ frontend/src/views/BooksView.vue
@@ -8,47 +8,33 @@
   </section>
 </template>
 
-<script>
+
+<script setup>
 import SectionHeader from "@/components/SectionHeader.vue";
 import BookList from "@/components/BookList.vue";
 import PaginationWidget from "@/components/widgets/PaginationWidget.vue";
 import {useBookStore} from "@/stores/bookStore.js";
-import {mapState} from "pinia";
+import {computed, ref} from "vue";
 
-export default {
-  name: "BooksView",
-  components: {
-    SectionHeader,
-    BookList,
-    PaginationWidget
-  },
-  data() {
-    return {
-      sectionTitle: "Kitaplar",
-      currentPage: 1,
-      itemsPerPage: 8,
-    }
-  },
-  computed: {
-    ...mapState(useBookStore, ['books']),
+const sectionTitle = ref('Kitaplar')
+const currentPage = ref(1)
+const itemsPerPage = ref(8)
 
-    totalPages() {
-      return Math.ceil(this.books.length / this.itemsPerPage)
-    },
-    paginatedBooks() {
-      const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-      const endIndex = startIndex + this.itemsPerPage;
-      return this.books.slice(startIndex, endIndex);
-    }
-  },
-  methods: {
-    updatePage(page) {
-      this.currentPage = page
-    },
-  },
+const bookStore = useBookStore();
 
-}
+const totalPages = computed(() => Math.ceil(bookStore.books.length / itemsPerPage.value));
+
+const paginatedBooks = computed(() => {
+  const startIndex = (currentPage.value - 1) * itemsPerPage.value;
+  const endIndex = startIndex + itemsPerPage.value;
+  return bookStore.books.slice(startIndex, endIndex);
+});
+
+const updatePage = (page) => {
+  currentPage.value = page;
+};
 </script>
+
 
 <style scoped>
 
